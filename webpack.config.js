fb1239bec010d9e8d5bd1b31a6ca6dabe1325d45
Pascal, Kenny <Kenny_Pascal@s5a.com>
2019-03-19 21:33:09 -0400

    adding inline source types

--- webpack.config.js
+++ webpack.config.js
@@ -105,6 +105,7 @@ module.exports = {
 
     // html
     new HtmlWebpackPlugin({
+      inlineSource: '.(js|css)$',
       chunks: ['app'],
       template: 'index.ejs',
       filename: 'index.html',
