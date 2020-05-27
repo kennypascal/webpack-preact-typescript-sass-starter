4d0aca04d3b072f812e5934fca3d6f981e4e6387
Kenny Pascal <kp@kennypascal.com>
2020-05-27 15:23:13 -0400

    replaced postcss-next with postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -9,11 +9,11 @@ const isProduction = process.argv.indexOf('-p') >= 0;
 
 const postcssImport = require('postcss-import')({ addDependencyTo: webpack, path: ['node_modules', 'src'] });
 const postcssUrl = require('postcss-url')();
-const postcssCssNext = require('postcss-cssnext')({ browsers: ['last 2 versions', 'ie >= 9', 'iOS 7'] });
+const postcssPresetEnv = require('postcss-preset-env');({ browsers: ['last 2 versions', 'ie >= 9', 'iOS 7'] });
 const postcssCssReporter = require('postcss-reporter')({ clearReportedMessages: true });
 const postcssCssDiscardComments = require('postcss-discard-comments')({ removeAll: true });
 const postcssCssBrowserReporter = require('postcss-browser-reporter')({ disabled: isProduction });
 
 module.exports = {
-  plugins: [postcssImport, postcssUrl, postcssCssNext, postcssCssReporter, postcssCssDiscardComments, postcssCssBrowserReporter],
+  plugins: [postcssImport, postcssUrl, postcssPresetEnv, postcssCssReporter, postcssCssDiscardComments, postcssCssBrowserReporter],
 };
